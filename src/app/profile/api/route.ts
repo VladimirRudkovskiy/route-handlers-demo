4a4de5c6f38178f3e9cd1586f7f3fae3437ce439
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-05-27 14:20:40 +0300

    headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-	return new Response ('Profile API data');
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+
+export async function GET(request: NextRequest) {
+	// const requestHeaders = new Headers(request.headers); //extract the request headers
+	// console.log(requestHeaders.get("Authorization")); //log the Authorization header
+
+	const headersList = await headers();
+	console.log(headersList.get("Authorization")); //log the Authorization header
+
+	return new Response("<h1>Profile API data</h1>", {
+		headers: {
+			"Content-Type": "text-html",
+		},
+	})
 }
