ec50f241a3f263b1a781a0e7e0c279ddf6c2f627
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-05-27 13:43:10 +0300

    comments for prev push

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,8 +1,8 @@
 import { comments } from "../data"
 
 export async function GET(_request: Request, {params}: {params: Promise<{id: string}>}) {
-	const { id } = await params
-	const comment = comments.find((comment) => comment.id === parseInt(id));
+	const { id } = await params //extracting id from the parameters
+	const comment = comments.find((comment) => comment.id === parseInt(id)); //finding data co-responding to that id
 
-	return Response.json(comment);
+	return Response.json(comment); //return as json
 }
