084bc6538a794f30a12db36303970d946ec8fb15
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-05-27 13:51:13 +0300

    handling PATCH requests

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -6,3 +6,16 @@ export async function GET(_request: Request, {params}: {params: Promise<{id: str
 
 	return Response.json(comment); //return as json
 }
+
+
+export async function PATCH(request: Request, {params}: {params: Promise<{id: string}>}) {
+	const { id } = await params; //extracting id from the parameters
+	const body = await request.json(); //getting the request body
+	const { text } = body; //extracting text from the body
+
+	const index = comments.findIndex((comment) => comment.id === parseInt(id)); //finding the index of the comment with that id
+
+	comments[index].text = text; //updating the text of the comment
+	return Response.json(comments[index]); //returning the upodated comment as json
+}
+
